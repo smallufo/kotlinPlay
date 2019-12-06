e82374f18bf9211ee46b56555c2c392b20550fb9
Smallufo Huang <smallufo@gmail.com>
2019-12-06 17:29:12 +0800

    play batch

--- batching/src/main/java/foobar/RecordsReader.kt
+++ batching/src/main/java/foobar/RecordsReader.kt
@@ -3,6 +3,19 @@
  */
 package foobar
 
+data class Record(val region: String,
+                  val country: String,
+                  val itemType: String,
+                  val salesChannel: String,
+                  val orderPriority: String ,
+                  val orderDate : String,
+                  val shipDate : String,
+                  val unitsSold : Int,
+                  val unitPrice : Double ,
+                  val unitCost : Double,
+                  val totalCost : Double,
+                  val totalProfit : Double)
+
 class RecordsReader {
 
 }
