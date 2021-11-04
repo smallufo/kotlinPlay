91257bece68540c0152500668866b5db2ce13d05
Smallufo Huang <smallufo@gmail.com>
2021-11-04 17:02:59 +0800

    some pom updates

--- rsocketPlay/src/test/java/ExampleClient.kt
+++ rsocketPlay/src/test/java/ExampleClient.kt
@@ -3,20 +3,21 @@
  */
 
 import io.rsocket.Payload
-import io.rsocket.RSocketFactory
+import io.rsocket.RSocket
+import io.rsocket.core.RSocketConnector
 import io.rsocket.transport.netty.client.WebsocketClientTransport
 import io.rsocket.util.DefaultPayload
 import reactor.core.publisher.Flux
 import java.net.URI
-import java.time.Duration
 
 object ExampleClient {
   @JvmStatic
   fun main(args: Array<String>) {
     val ws = WebsocketClientTransport.create(URI.create("ws://rsocket-demo.herokuapp.com/ws"))
-    val client = RSocketFactory.connect()
-      .keepAlive(Duration.ofSeconds(20), Duration.ofSeconds(30), 3)
-      .transport(ws).start().block()!!
+    val client: RSocket = RSocketConnector.connectWith(ws).block()!!
+//    val client = RSocketFactory.connect()
+//      .keepAlive(Duration.ofSeconds(20), Duration.ofSeconds(30), 3)
+//      .transport(ws).start().block()!!
 
     try {
       val s: Flux<Payload> = client.requestStream(DefaultPayload.create("peace"))
